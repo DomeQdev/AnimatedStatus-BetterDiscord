01e333a58af87b66cdc9e020f0a0ae1265195150
DomeQ <45435431+DomeQdev@users.noreply.github.com>
2020-06-18 09:42:03 +0200

    Update AnimateStatus.plugin.js

--- AnimateStatus.plugin.js
+++ AnimateStatus.plugin.js
@@ -1,4 +1,4 @@
-//META{"name":"AnimatedStatus","authorId":"714550610489508050","invite":"netflix","donate":"https://domeq.dev/donate","website":"https://domeq.dev/"}*// 
+//META{"name":"AnimatedStatus","authorId":"714550610489508050","invite":"PxAhJKa","donate":"https://anyialt.xyz/donate","website":"https://domeq.dev/"}*// 
 
 
 class AnimatedStatus {
